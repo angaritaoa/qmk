8e33bb2ab373c120c4ccd83d68dcafb9af71fa6a
Andres Angarita <angaritaoa@gmail.com>
2022-06-28 11:51:39 -0500

    Actualización general de todos los archivos

--- rules.mk
+++ rules.mk
@@ -13,4 +13,4 @@ RGBLIGHT_ENABLE		= no	# Enable WS2812 RGB underlight.
 AUTO_SHIFT_ENABLE	= no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE	= no	# Breathing sleep LED during USB suspend
